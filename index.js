d777495824392c8711a11dc7e18087449d145bc3
Vodopyanov Egor <egorvodopyanov@gmail.com>
2022-05-11 01:12:54 +0300

    corrupted file

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const run = async () => {
   const buffer = await sharp('./image.png')
     .png({ compressionLevel: 9, quality: 90 })
     .toBuffer()
-    .catch((e) => console.log(e))
 
   return sharp(buffer)
     .toFile('./out.png')
-    .catch((e) => console.log(e))
 }
 
-run()
+try {
+  run()
+} catch(e) {
+  console.log(e)
+}
