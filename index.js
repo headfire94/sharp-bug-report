711dc8e8883c58ad6c32eea2c9369db0fcaa1edb
Vodopyanov Egor <egorvodopyanov@gmail.com>
2022-05-11 14:42:51 +0300

    process two different PNG with alpha channel

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const sharp = require('sharp')
 
-const run = async () => {
-  const buffer = await sharp('./image.png')
+const processImg =async  filename => {
+  console.log('processing', filename)
+  const buffer = await sharp(`./${filename}.png`)
     .png({ compressionLevel: 9, quality: 90 })
     .toBuffer()
-
+  
   return sharp(buffer)
-    .toFile('./out.png')
+    .toFile(`./${filename}-out.png`)
+}
+
+const run = async () => {
+  ['image-2', 'image'].map(processImg)
 }
 
 try {
